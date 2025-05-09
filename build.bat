254146c5c6f5d21e1128644bd6b3948f46f20515
Andrew Kaufman <akaufman@nvidia.com>
2025-05-08 19:35:33 -0700

    Build : Use poetry and poethepoet workflow
    Replaces the need for so many shell scripts
    
    See OSRB 5270968

--- build.bat
+++ build.bat
@@ -1,51 +1,32 @@
 @echo off
 
-set "SCRIPT_DIR=%~dp0"
-set "BUILD_VENV=%SCRIPT_DIR%.build_venv"
-set "RUNTIME_VENV=%SCRIPT_DIR%.runtime_venv"
-set "PIP_INDEX_URL=https://REDACTED/simple"
+set VENV="%~dp0.venv"
 
-if "%1"=="-cc" (
-    echo Cleaning everything...
-    rd /s /q "%BUILD_VENV%"
-    rd /s /q "%RUNTIME_VENV%"
-    rd /s /q dist
-) else if "%1"=="-c" (
-    echo Cleaning distro and runtime...
-    rd /s /q "%RUNTIME_VENV%"
-    rd /s /q dist
+if "%1"=="-c" (
+    echo Cleaning...
+    if exist "%VENV%" (
+        rd /s /q "%VENV%"
+    )
+    if exist dist (
+        rd /s /q dist
+    )
 )
 
 REM setup the build environment
-if exist "%BUILD_VENV%\" (
-    call %BUILD_VENV%\Scripts\activate.bat
+if exist "%VENV%\" (
+    call %VENV%\Scripts\activate.bat
 ) else (
-    echo "Building: %BUILD_VENV%"
-    python -m venv "%BUILD_VENV%"
-    call %BUILD_VENV%\Scripts\activate.bat
+    echo "Building: %VENV%"
+    python -m venv "%VENV%"
+    call %VENV%\Scripts\activate.bat
     python -m pip install poetry
 )
 
 REM do the build
 poetry --version
-if exist "%SCRIPT_DIR%dist\" (
-    rd /s /q "%SCRIPT_DIR%dist"
+if exist "dist" (
+    rd /s /q "dist"
 )
 poetry build --format=wheel
 poetry lock
-
-REM prepare the runtime environment
-if exist "%RUNTIME_VENV%\" (
-    call %RUNTIME_VENV%\Scripts\activate.bat
-    REM install the artifact to the runtime
-    for %%a in ("%SCRIPT_DIR%dist\mjc_usd_converter-*.whl") do (
-        python -m pip install "%%a" --no-deps --force-reinstall
-    )
-) else (
-    echo Building: %RUNTIME_VENV%
-    python -m venv "%RUNTIME_VENV%"
-    call %RUNTIME_VENV%\Scripts\activate.bat
-    for %%a in ("%SCRIPT_DIR%dist\mjc_usd_converter-*.whl") do (
-        python -m pip install "%%a"
-    )
-)
+poetry install
