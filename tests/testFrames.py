c1d0d9f259f2de4c2bfa98ebf93913dc0eca18dc
Lou Rohan <lrohan@nvidia.com>
2025-06-12 13:04:14 -0700

    Fix transform baking issues

---
+++ tests/testFrames.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+import shutil
+import unittest
+
+from pxr import Gf, Sdf, Usd, UsdGeom
+
+import mjc_usd_converter
+
+
+class TestFrames(unittest.TestCase):
+    def tearDown(self):
+        if pathlib.Path("tests/output").exists():
+            shutil.rmtree("tests/output")
+
+    def assert_rotation_almost_equal(self, rot1, rot2, tolerance):
+        self.assertTrue(Gf.IsClose(rot1.GetAxis(), rot2.GetAxis(), tolerance))
+        self.assertTrue(Gf.IsClose(rot1.GetAngle(), rot2.GetAngle(), tolerance))
+
+    def test_frames(self):
+        tolerance = 1e-6
+        model = pathlib.Path("./tests/data/frames.xml")
+        model_name = pathlib.Path(model).stem
+        asset: Sdf.AssetPath = mjc_usd_converter.Converter().convert(model, pathlib.Path(f"tests/output/{model_name}"))
+        stage: Usd.Stage = Usd.Stage.Open(asset.path)
+
+        geom: Usd.Prim = stage.GetPrimAtPath("/frames/Geometry/mainBody/framedBody/ObjBox")
+        geom_mesh = UsdGeom.Mesh(geom)
+        self.assertTrue(geom_mesh)
+
+        transform = Gf.Transform(geom_mesh.ComputeLocalToWorldTransform(Usd.TimeCode.Default()))
+
+        self.assertTrue(Gf.IsClose(transform.GetTranslation(), Gf.Vec3d(-pow(2, 0.5) / 2, -pow(2, 0.5) / 2, 0), tolerance))
+        self.assert_rotation_almost_equal(transform.GetRotation(), Gf.Rotation(Gf.Vec3d(0, 0, 1), 90), 1e-4)
+        self.assertTrue(Gf.IsClose(transform.GetScale(), Gf.Vec3d(0.2, 0.2, 0.8), tolerance))
+
+        geom: Usd.Prim = stage.GetPrimAtPath("/frames/Geometry/mainBody/framedGeom")
+        geom_cube = UsdGeom.Cube(geom)
+        self.assertTrue(geom_cube)
+
+        transform = Gf.Transform(geom_cube.ComputeLocalToWorldTransform(Usd.TimeCode.Default()))
+
+        self.assertTrue(Gf.IsClose(transform.GetTranslation(), Gf.Vec3d(pow(2, 0.5) / 2, -pow(2, 0.5) / 2, 0), tolerance))
+        self.assert_rotation_almost_equal(transform.GetRotation(), Gf.Rotation(Gf.Vec3d(0, 0, 1), 90), 1e-4)
+        self.assertTrue(Gf.IsClose(transform.GetScale(), Gf.Vec3d(0.1, 0.1, 0.4), tolerance))
