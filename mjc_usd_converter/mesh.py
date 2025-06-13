8a8174dd8b1377100e93e86e6b9796c8dcb5048a
Lou Rohan <lrohan@nvidia.com>
2025-06-13 15:16:02 -0500

    Only issue a warning about multiple shapes and convert the first shape

--- mjc_usd_converter/mesh.py
+++ mjc_usd_converter/mesh.py
@@ -104,9 +104,10 @@ def __convert_obj(prim: Usd.Prim, input_path: pathlib.Path) -> UsdGeom.Mesh:
         Tf.RaiseRuntimeError(f'Invalid input_path: "{input_path}" could not be parsed. {reader.Error()}')
 
     shapes = reader.GetShapes()
-    # FUTURE: support multiple meshes in a single file (asset: leap_hand)
-    if len(shapes) != 1:
-        Tf.RaiseRuntimeError(f'Invalid input_path: "{input_path}" must contain a single mesh object')
+    if len(shapes) == 0:
+        Tf.RaiseRuntimeError(f'Invalid input_path: "{input_path}" contains no meshes')
+    elif len(shapes) > 1:
+        Tf.Warn(f'"{input_path}" contains multiple meshes, only the first one will be converted')
 
     attrib = reader.GetAttrib()
     obj_mesh = shapes[0].mesh
