cbea8c8194be003cb215e0e801010ce162a7bef9
Andrew Kaufman <akaufman@nvidia.com>
2025-07-30 16:40:37 -0700

    Enabled Atomic Asset validation

--- tests/util/ConverterTestCase.py
+++ tests/util/ConverterTestCase.py
@@ -26,3 +26,9 @@ class ConverterTestCase(usdex.test.TestCase):
     def assert_rotation_almost_equal(self, rot1: Gf.Rotation, rot2: Gf.Rotation, tolerance: float = 1e-6):
         self.assertTrue(Gf.IsClose(rot1.GetAxis(), rot2.GetAxis(), tolerance), f"Axis mismatch: {rot1.GetAxis()} != {rot2.GetAxis()}")
         self.assertTrue(Gf.IsClose(rot1.GetAngle(), rot2.GetAngle(), tolerance), f"Angle mismatch: {rot1.GetAngle()} != {rot2.GetAngle()}")
+
+    def setUp(self):
+        super().setUp()
+        # All conversion results should be valid atomic assets
+        self.validationEngine.enable_rule(omni.asset_validator.AnchoredAssetPathsChecker)
+        self.validationEngine.enable_rule(omni.asset_validator.SupportedFileTypesChecker)
