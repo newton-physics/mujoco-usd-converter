1086e982b9d3d4a92d4a9ee9aa3631eab979f5a2
Andrew Kaufman <akaufman@nvidia.com>
2025-08-06 17:35:30 -0700

    Bump version

--- mujoco_usd_converter/_version.py
+++ mujoco_usd_converter/_version.py
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-__version__ = "0.1.0.dev6"
+__version__ = "0.1.0.dev7"
