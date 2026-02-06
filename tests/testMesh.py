c82f9a9e37375d20d36bbac25fd7bf0f133c6298
Yutaka Yoshisaka <yyoshisaka@nvidia.com>
2026-02-06 13:09:31 +0900

    obj: ngons

--- tests/testMesh.py
+++ tests/testMesh.py
@@ -40,18 +40,22 @@ class TestMesh(ConverterTestCase):
         # meshes are references to the geometry library layer
         self.assertTrue(obj_mesh_prim.HasAuthoredReferences())
         usd_mesh_obj = UsdGeom.Mesh(obj_mesh_prim)
-        self.assertTrue(usd_mesh_obj.GetPointsAttr().HasAuthoredValue())
-        self.assertTrue(usd_mesh_obj.GetFaceVertexCountsAttr().HasAuthoredValue())
-        self.assertTrue(usd_mesh_obj.GetFaceVertexIndicesAttr().HasAuthoredValue())
+        self.assertEqual(len(usd_mesh_obj.GetPointsAttr().Get()), 8)
+        face_vertex_counts = usd_mesh_obj.GetFaceVertexCountsAttr().Get()
+        self.assertEqual(len(face_vertex_counts), 6)
+        self.assertTrue(all(c == 4 for c in face_vertex_counts))
+        face_vertex_indices = usd_mesh_obj.GetFaceVertexIndicesAttr().Get()
+        self.assertEqual(len(face_vertex_indices), 24)
+
         # The sample box.obj has normals and UVs
         normals_primvar: UsdGeom.Primvar = UsdGeom.PrimvarsAPI(usd_mesh_obj).GetPrimvar("normals")
         self.assertTrue(normals_primvar.IsDefined())
-        self.assertTrue(normals_primvar.HasAuthoredValue())
-        self.assertTrue(normals_primvar.GetIndicesAttr().HasAuthoredValue())
+        self.assertEqual(len(normals_primvar.GetAttr().Get()), 6)
+        self.assertEqual(len(normals_primvar.GetIndicesAttr().Get()), 24)
         uvs_primvar: UsdGeom.Primvar = UsdGeom.PrimvarsAPI(usd_mesh_obj).GetPrimvar("st")
         self.assertTrue(uvs_primvar.IsDefined())
-        self.assertTrue(uvs_primvar.HasAuthoredValue())
-        self.assertTrue(uvs_primvar.GetIndicesAttr().HasAuthoredValue())
+        self.assertEqual(len(uvs_primvar.GetAttr().Get()), 4)
+        self.assertEqual(len(uvs_primvar.GetIndicesAttr().Get()), 24)
 
         # Test mesh conversion with no name
         mesh_prim: Usd.Prim = stage.GetPrimAtPath(f"/{model_name}/Geometry/body1/body2/box")
@@ -59,6 +63,19 @@ class TestMesh(ConverterTestCase):
         # meshes are references to the geometry library layer
         self.assertTrue(mesh_prim.HasAuthoredReferences())
         usd_mesh = UsdGeom.Mesh(mesh_prim)
-        self.assertTrue(usd_mesh.GetPointsAttr().HasAuthoredValue())
-        self.assertTrue(usd_mesh.GetFaceVertexCountsAttr().HasAuthoredValue())
-        self.assertTrue(usd_mesh.GetFaceVertexIndicesAttr().HasAuthoredValue())
+        self.assertEqual(len(usd_mesh.GetPointsAttr().Get()), 8)
+        face_vertex_counts = usd_mesh.GetFaceVertexCountsAttr().Get()
+        self.assertEqual(len(face_vertex_counts), 6)
+        self.assertTrue(all(c == 4 for c in face_vertex_counts))
+        face_vertex_indices = usd_mesh.GetFaceVertexIndicesAttr().Get()
+        self.assertEqual(len(face_vertex_indices), 24)
+
+        # The sample box.obj has normals and UVs
+        normals_primvar: UsdGeom.Primvar = UsdGeom.PrimvarsAPI(usd_mesh).GetPrimvar("normals")
+        self.assertTrue(normals_primvar.IsDefined())
+        self.assertEqual(len(normals_primvar.GetAttr().Get()), 6)
+        self.assertEqual(len(normals_primvar.GetIndicesAttr().Get()), 24)
+        uvs_primvar: UsdGeom.Primvar = UsdGeom.PrimvarsAPI(usd_mesh).GetPrimvar("st")
+        self.assertTrue(uvs_primvar.IsDefined())
+        self.assertEqual(len(uvs_primvar.GetAttr().Get()), 4)
+        self.assertEqual(len(uvs_primvar.GetIndicesAttr().Get()), 24)
